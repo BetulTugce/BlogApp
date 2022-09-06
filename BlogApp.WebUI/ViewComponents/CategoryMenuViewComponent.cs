5922d8d79fe3c9228c3a88979628088a350a206d
Betül Tuğçe Dikdoğmuş <79140478+BetulTugce@users.noreply.github.com>
2022-09-07 00:04:09 +0300

    Add files via upload
    adding webui layer

---
+++ BlogApp.WebUI/ViewComponents/CategoryMenuViewComponent.cs
@@ -0,0 +1,23 @@
+﻿using BlogApp.Data.Abstract;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogApp.WebUI.ViewComponents
+{
+    public class CategoryMenuViewComponent : ViewComponent
+    {
+        private ICategoryRepository _repository;
+        public CategoryMenuViewComponent(ICategoryRepository repository)
+        {
+            _repository = repository;
+        }
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.SelectedCategory = RouteData?.Values["id"]; //route data null değilse id değerini alıp seçilen kategoriyi seçili hale getirecek.
+            return View(_repository.GetAll());
+        }
+    }
+}
